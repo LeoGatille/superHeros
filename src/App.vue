93e37adc7d58688d46bdd4ab9f9e882c790115be
Leo <leo@initiation.com>
2021-01-05 09:45:39 +0100

    Placed the nav

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,71 @@
 <template>
-  <div id="app">
-    <NavBar/>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-app-bar
+        fixed
+        prominent
+        color="#fcb69f"
+        dark
+        shrink-on-scroll
+        fade-img-on-scroll
+        src="https://picsum.photos/1920/1080?random"
+    >
+      <template v-slot:img="{ props }">
+        <v-img
+            v-bind="props"
+            gradient="to top right, rgba(19,84,122,.5), rgba(128,208,199,.8)"
+        ></v-img>
+      </template>
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+      <v-app-bar-title>Title</v-app-bar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-dots-vertical</v-icon>
+      </v-btn>
+      <template v-slot:extension>
+        <v-tabs align-with-title>
+          <v-tab>Tab 1</v-tab>
+          <v-tab>Tab 2</v-tab>
+          <v-tab>Tab 3</v-tab>
+        </v-tabs>
+      </template>
+    </v-app-bar>
+    <v-main>
+      <v-sheet
+          id="scrolling-techniques-2"
+          class="overflow-y-auto"
+      >
+        <v-container>
+          <div style="width: 100%; height: 2000px; background: red"></div>
+        </v-container>
+      </v-sheet>
+    </v-main>
+  </v-app>
 </template>
+
 <script>
-  import NavBar from "@/components/NavBar";
-  export default {
-    components: {
-      NavBar
-    }
-  }
+// import HelloWorld from './components/HelloWorld';
+// import NavBar from "@/components/NavBar";
+
+export default {
+  name: 'App',
+
+  components: {
+    // NavBar,
+  },
+
+  data: () => ({
+    //
+  }),
+};
 </script>
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,7 +1,62 @@
 <template>
-  <main>
-    <img height="50" src="../../public/logo.jpg" alt="le logo des vrai héros">
-  </main>
+<div>
+    <v-app-bar
+        absolute
+        color="#f50000"
+        dark
+        shrink-on-scroll
+        prominent
+        src="../../public/all_heroes.png"
+        fade-img-on-scroll
+        scroll-target="#scrolling-techniques-3"
+    >
+      <template v-slot:img="{ props }">
+        <v-img
+            v-bind="props"
+            gradient="to top right, rgba(245, 00, 00, 0.7), rgba(245, 00, 00, 0.3)"
+        ></v-img>
+      </template>
+
+<!--      <v-app-bar-nav-icon></v-app-bar-nav-icon>-->
+
+      <v-app-bar-title>
+        You are my hero
+      </v-app-bar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-dots-vertical</v-icon>
+      </v-btn>
+
+      <template v-slot:extension>
+        <v-tabs align-with-title>
+          <v-tab>Tab 1</v-tab>
+          <v-tab>Tab 2</v-tab>
+          <v-tab>Tab 3</v-tab>
+        </v-tabs>
+      </template>
+    </v-app-bar>
+    <v-sheet
+        id="scrolling-techniques-3"
+        class="overflow-y-auto"
+        max-height="600"
+    >
+      <v-container style="height: 1000px;"></v-container>
+    </v-sheet>
+</div>
+
+<!--  <main>-->
+<!--    <img height="50" src="../../public/logo.jpg" alt="le logo des vrai héros">-->
+<!--  </main>-->
 </template>
 
 <script>
