d17737de0443a401a4337ce2672038cf3b484711
leo <leo.e.gatille@gmail.com>
2021-01-20 13:17:18 +0100

    fix tab active
     the active tab is now set with current route

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,56 +1,65 @@
 <template>
-<div class="navbar-container">
-  <v-app-bar
-      app
-      fixed
-      prominent
-      short
-      color="#fc2803"
-      dark
-      shrink-on-scroll
-      fade-img-on-scroll
-      src="../../public/all_heroes.png"
-  >
-    <template v-slot:img="{ props }">
-      <v-img
-          v-bind="props"
-          gradient="to top right, rgba(245, 00, 00, 0.7), rgba(252, 165, 3, 0.3)"
-      ></v-img>
-    </template>
+  <div class="navbar-container">
+    <v-app-bar
+        app
+        fixed
+        prominent
+        short
+        color="#fc2803"
+        dark
+        shrink-on-scroll
+        fade-img-on-scroll
+        src="../../public/all_heroes.png"
+    >
+      <template v-slot:img="{ props }">
+        <v-img
+            v-bind="props"
+            gradient="to top right, rgba(245, 00, 00, 0.7), rgba(252, 165, 3, 0.3)"
+        ></v-img>
+      </template>
 
 
-    <v-app-bar-title>I need a hero</v-app-bar-title>
+      <v-app-bar-title>I need a hero</v-app-bar-title>
 
-    <v-spacer></v-spacer>
+      <v-spacer></v-spacer>
 
-    <LangSelector/>
-<!--    <v-btn icon @click="setSettingsDisplay">-->
-<!--      <font-awesome-icon :icon="['fas', 'cog']" />-->
-<!--    </v-btn>-->
+      <LangSelector/>
+      <!--    <v-btn icon @click="setSettingsDisplay">-->
+      <!--      <font-awesome-icon :icon="['fas', 'cog']" />-->
+      <!--    </v-btn>-->
 
-    <template v-slot:extension>
-      <v-tabs>
-        <router-link :to="{ path: '/favorites' }">
-          <v-tab>{{ $t('tabs.dashboard')}}</v-tab>
-        </router-link>
-        <router-link :to="{ path: '/', }">
-          <v-tab>{{ $t('tabs.heroes')}}</v-tab>
-        </router-link>
-      </v-tabs>
-    </template>
-  </v-app-bar>
-</div>
+      <template v-slot:extension>
+        <v-tabs v-model="selectedTab">
+           <v-tab  v-for="tab in tabs" :key="tab.id" :to="tab.path">{{tab.name}}</v-tab>
+        </v-tabs>
+      </template>
+    </v-app-bar>
+  </div>
 </template>
 
 <script>
 import LangSelector from '@/components/LangSelector'
 
 export default {
-name: "NavBar",
+  name: "NavBar",
   components: {
     LangSelector
   },
+  data() {
+    return {
+      selectedTab: 1
+    }
+  },
+  computed: {
+    tabs() {
+      return [
+        {index: 1, name: this.$t('tabs.heroes'), path: '/'},
+        {index: 2, name: this.$t('tabs.dashboard'), path: '/favorites'},
+      ]
+    }
+  },
   methods: {
+
     setSettingsDisplay() {
       this.$store.dispatch('setSettingsDisplay')
     }
@@ -63,6 +72,7 @@ name: "NavBar",
 .navbar-container {
   z-index: 99999;
 }
+
 main {
   width: 100%;
   height: 70px;
