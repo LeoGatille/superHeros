a625df803d77af3737b5605993d6844bc2b100f3
leo <leo.e.gatille@gmail.com>
2021-01-21 12:04:14 +0100

    fix Pagination.vue
    simpler && better

--- src/components/Pagination.vue
+++ src/components/Pagination.vue
@@ -1,22 +1,64 @@
 <template>
   <div class="pagination-container">
-    <v-btn disabled icon v-if="((currentPage + 1) > 3)">...</v-btn>
-    <span v-for="index in pagesBeforeCurrentPage" :key="'beforeCurrentPage' + index">
-          <v-btn icon @click="changePage(index)">{{ index + 1 }}</v-btn>
+  <span v-show="(currentPage ) > 3 ">
+      <v-btn
+          icon
+          @click="changePage(0)"
+      >
+        {{ 1 }}
+      </v-btn>
+      <v-btn
+          disabled
+          icon
+      >
+    ...
+  </v-btn>
+    </span>
+    <span
+        v-for="(index, i) in 3"
+        :key="'beforeCurrentPage' + index"
+    >
+          <v-btn
+              v- v-if="((i - 3) + (currentPage + 1)) > 0"
+              icon
+              @click="changePage((i - 3) + (currentPage))"
+          >
+            {{ (i - 3) + (currentPage + 1) }}
+          </v-btn>
     </span>
 
-    <v-btn v-if="currentPage < 0 " icon @click="changePage(currentPage - 1)">
-      <font-awesome-icon :icon="['fas', 'arrow-left']"/>
-    </v-btn>
-    <v-btn icon color="black" class="current-page">{{ currentPage + 1 }}</v-btn>
-    <v-btn v-if="currentPage > maxPage" icon @click="changePage(currentPage + 1 )">
-      <font-awesome-icon :icon="['fas', 'arrow-right']"/>
+    <v-btn
+        icon
+        color="black"
+        class="current-page"
+    >
+      {{ currentPage + 1 }}
     </v-btn>
 
-    <span v-for="index in pagesAfterCurrentPage" :key="'afterCurrentPage' + index">
-          <v-btn icon @click="changePage(index )">{{ index + 1 }}</v-btn>
+    <span v-for="(index, i) in 3" :key="'afterCurrentPage' + index">
+        <v-btn
+            v-if="((maxPage - currentPage) > (i + 1))"
+            icon
+            @click="changePage(currentPage + (i + 1))"
+        >
+          {{ (currentPage + i) + 2 }}
+        </v-btn>
     </span>
-    <v-btn disabled icon v-if="maxPage > (currentPage + 3)">...</v-btn>
+
+    <span v-show="maxPage > (currentPage + 3)">
+      <v-btn
+          disabled
+          icon
+      >
+        ...
+      </v-btn>
+      <v-btn
+          icon
+          @click="changePage(maxPage - 1)"
+      >
+        {{ maxPage }}
+      </v-btn>
+    </span>
   </div>
 </template>
 
@@ -33,29 +75,10 @@ export default {
   },
   computed: {
     ...mapState(['maxPage']),
-    pagesBeforeCurrentPage() {
-      const indexBeforeCurrentPage = [];
-      let i = this.currentPage - 4;
-      while ((this.currentPage > 0) && (i < this.currentPage - 1)) {
-        i++;
-        if (i > -1) {
-          indexBeforeCurrentPage.push(i);
-        }
-      }
-      return indexBeforeCurrentPage
-    },
-    pagesAfterCurrentPage() {
-      const indexAfterCurrentPage = [];
-      let i = this.currentPage;
-      while ((this.maxPage > 1) && (i >= this.currentPage) && (i < this.currentPage + 3) && (this.maxPage > i)) {
-        i++;
-        indexAfterCurrentPage.push(i);
-      }
-      return indexAfterCurrentPage
-    },
   },
   methods: {
     changePage(index) {
+      console.log(index)
       this.$emit('changePage', index)
     }
   }
